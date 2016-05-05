29ca5ac0f94272d7fb999ab7d61057da86f5a399
nerve <nero-add@mail.ru>
2016-05-05 20:44:09 +0900

    first initialisation

---
+++ Jitsu/gen/ru/nerverid/jitsu/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.nerverid.jitsu;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Jitsu/src/ru/nerverid/jitsu/MyMainScreen.java
@@ -0,0 +1,27 @@
+package ru.nerverid.jitsu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MyMainScreen extends Activity 
+{
+	protected void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_main_activity);
+	}
+	
+	 
+ //   public void onClick (View view)
+ //   {
+ //   	Intent intent = new Intent (MyMainScreen.this, Technics.class);
+ //   	startActivity(intent);
+ //   }
+
+
+
+
+}
