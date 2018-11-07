6a9c92b1698a2f4e3e686a3ed32ff5e8194498fe
nerve <nero-add@mail.ru>
2018-11-07 14:27:51 +0900

    Create stucture

---
+++ CircularLinkedList/src/com/blogspot/nerverid/CircularLinkedList.java
@@ -0,0 +1,18 @@
+package com.blogspot.nerverid;
+
+public class CircularLinkedList {
+
+	Node head;
+	Node tail;
+	
+	class Node{
+		int data;
+		Node next;
+		Node prev;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
