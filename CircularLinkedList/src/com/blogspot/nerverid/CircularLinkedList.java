eee34d7279b61ed6f96c0b40c85bc01ea4e60085
nerve <nero-add@mail.ru>
2018-11-09 11:16:37 +0900

    Added all tests'

--- CircularLinkedList/src/com/blogspot/nerverid/CircularLinkedList.java
+++ CircularLinkedList/src/com/blogspot/nerverid/CircularLinkedList.java
@@ -19,12 +19,21 @@ public class CircularLinkedList {
 		// Тестируем добавление элемента
 		cll.add(2);
 		cll.add(4);
-		if (cll.count()> 1) System.out.println("Elements added. Элементы добавлены. Тест пройден");
+		if (cll.count()> 1) System.out.println("Elements added. Test complit! Элементы добавлены. Тест пройден");
 		
 		// Test for delete element into set
 		// Тестируем удаление элемента
 		cll.delete(4);
-		if (cll.count()<2) System.out.printkn("Element delete. Элемент удалён. Тест пройден!")
+		if (cll.count()<2) System.out.printkn("Element delete. Test complit! Элемент удалён. Тест пройден!")
+		
+		// Test inserting new element after selected element
+		// Тестируем вставку элементов после выбранного
+		cll.insertAfter(head.next, 5);
+		if (cll.count()>=2) System.out.println ("Element inserting. Test complit! Элемент вставлен. Тест пройден!");
+		// Test searching element
+		// Тестируем поиск элемента.
+		if (cll.search(5) != null) System.out.println("Element search complit. Test compit! Элемент найден. Тест пройден!");
+		
 	}
 
 }
