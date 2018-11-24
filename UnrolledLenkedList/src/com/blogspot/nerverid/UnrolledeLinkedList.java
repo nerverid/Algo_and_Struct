248cbe8f7eab6eed45c278efd1d4a1dd6a3f59ae
Пользователь <nero-add@mail.ru>
2018-11-24 21:18:27 +0900

    class UnrolledLinkedList, create two insert methods

--- UnrolledLenkedList/src/com/blogspot/nerverid/UnrolledeLinkedList.java
+++ UnrolledLenkedList/src/com/blogspot/nerverid/UnrolledeLinkedList.java
@@ -1,5 +1,7 @@
 package com.blogspot.nerverid;
 
+import java.util.Random;
+
 public class UnrolledeLinkedList {
 
 	Node head;
@@ -22,28 +24,62 @@ public class UnrolledeLinkedList {
 	
 	public void insertNode(int data){
 		Node n1 = new Node (data);
-		n1.dataArray = new int[5];
-		n1.dataArray[0]=1;
-		n1.dataArray[1]=2;
-		n1.dataArray[2]=3;
-		head = n1;
+		n1.dataArray = new int[5]; // How many elements to default
+		if (head == null) {
+			n1.dataArray[0]=1;
+			n1.dataArray[1]=2;
+			n1.dataArray[2]=3;
+			head = n1;
+		} else {
+			Node last = head;
+			while (last.next != null) {
+				last = last.next;
+			}
+			last.next = n1;
+		}
 	}
 	
 	public void insertNode(int data, int lengthArrayNew){
 		Node n1 = new Node (data);
 		n1.dataArray = new int[lengthArrayNew];
-		n1.dataArray[0]=1;
-		n1.dataArray[1]=2;
-		n1.dataArray[2]=3;
-		head = n1;
+		Random random = new Random();
+		if (head == null) {
+			for (int i =0 ; i< lengthArrayNew; i++) {
+				n1.dataArray[i] = random.nextInt(10);
+			}
+			head = n1;
+		} else {
+			Node last = head;
+			while (last.next != null) {
+				last = last.next;
+			}
+			last.next = n1;
+		}
 	}
 	
+	// Method for Tests. Count each elements 
+	// Метод для подсчёта всех элементов. Нужен для тестирования ваставки/удаления
+	public void countList() {
+		Node nextNode = head;
+		while (nextNode != null) {
+			System.out.println("Node - " + nextNode.data);
+			for(int i = 0; i < nextNode.dataArray.length; i++) {
+				System.out.print(" " + nextNode.dataArray[i]);
+			}
+			nextNode = nextNode.next;
+		}
+	}
+	
+	// This method for print each elements of List and elements of Nodes
+	// Метод для вывода каждого элемента списка, и каждого элемента узла списка
 	public void printList (){
 		Node nextNode = head;
 		while(nextNode != null){
+			System.out.println("Node - " + nextNode.data);
 			for (int i=0 ; i< nextNode.dataArray.length; i++){
 				System.out.print(" " + nextNode.dataArray[i]);
 			}
+			System.out.println("");
 			nextNode = nextNode.next;
 		}
 	}
@@ -55,7 +91,6 @@ public class UnrolledeLinkedList {
 		// Test for insert into empty class without it's methods.
 		// Тест на вставку в пустой класс без методов.
 		ull.insertNode(1);
-		ull.printList();
 		ull.insertNode(2, 8);
 		ull.printList();
 	}
