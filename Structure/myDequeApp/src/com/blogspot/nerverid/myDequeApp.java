e063c4d0d4a74dd097c350add85632dca49b8adb
nerve <nero-add@mail.ru>
2019-01-31 15:21:40 +0900

    Create Deque structure for my lerning, empty classes and integration tests

---
+++ Structure/myDequeApp/src/com/blogspot/nerverid/myDequeApp.java
@@ -0,0 +1,27 @@
+package com.blogspot.nerverid;
+
+class Node{
+	
+}
+
+class myDeque{
+	
+}
+
+public class myDequeApp {
+
+	public static void main(String[] args) {
+		myDeque deque = new myDeque();
+		deque.insert();
+		deque.peek();
+		deque.insertFront();
+		deque.peek();
+		deque.insertBack();
+		deque.peek();
+		deque.insert();
+		
+		deque.remove();
+		deque.peek();
+	}
+
+}
